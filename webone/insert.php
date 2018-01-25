37d816285eb6a1f13de246a09d89d32302a37aad
zahiruddin <kepaknaga@gmail.com>
2018-01-25 12:24:04 +0800

    add php file

---
+++ webone/insert.php
@@ -0,0 +1,32 @@
+<?php
+
+	$con = mysql_connect('localhost','root','norzain');
+
+	if(!$con)	#cek connection
+	{
+		echo 'Not connected to server';
+	}
+
+	if(!mysql_select_db('zebbo',$con))	#pilih database
+	{
+		echo 'Database not selected';
+	}
+
+	$Name = $_POST['username'];
+	$Email = $_POST['email'];
+
+	$sql = "INSERT INTO data(Name, Email) VALUES ('$Name','$Email')";
+
+	if(!mysql_query($sql,$con)) 
+	{
+		echo 'Not inserted';
+	}
+	else
+	{
+		echo 'Inserted';
+	}
+
+	#header("refresh:2; url=main.php");
+
+
+?>
---
+++ webone/main.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>
+insert data to mysql
+</title>
+</head>
+<body>
+
+<form action="insert.php" method="post">
+
+Name : <input type="text" name="username">
+<br/>
+Email : <input type="text" name="email">
+<br/>
+
+<input type="submit" name="Insert">
+
+</form>
+
+</body>
+</html>
